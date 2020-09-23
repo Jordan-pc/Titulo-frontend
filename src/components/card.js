6c2fd58baa0f5eb00e131869b870f0069ccd321f
Jordan <jordan.porras1@gmail.com>
2020-09-23 00:42:09 -0300

    initial commit

---
+++ src/components/card.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export const Card = () => {
+  return (
+    <div className='col-md-8'>
+      <div className='card mb-3 mt-3'>
+        <h5 className='card-header'>card title</h5>
+        <div className='card-body'>
+          <p>
+            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
+            ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+            lorem ipsum lorem ipsum
+          </p>
+          <a className='stretched-link' href='/'>
+            {' '}
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/navbar.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import logoutem from '../Logoutem-1.png';
+
+export const NavBar = () => {
+  return (
+    <nav
+      className='navbar navbar-expand navbar-light flex-column flex-md-row bd-navbar'
+      style={{ backgroundColor: 'white' }}
+    >
+      <a className='navbar-brand' href='/'>
+        <img
+          src={logoutem}
+          alt='LogoUtem'
+          style={{ width: '30px', height: '40px', marginRight: ' 5px' }}
+        ></img>
+        Recursos UTEM
+      </a>
+      <div className='navbar-nav-scroll'>
+        <ul className='navbar-nav bd-navbar-nav flex-row'>
+          <li className='nav-item'>
+            <a className='nav-link' href='/'>
+              Inicio
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link' href='/'>
+              Segunda
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link' href='/'>
+              Tercera
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className='navbar-nav ml-md-auto'>
+        <a
+          className='nav-link btn btn-outline-info action-button'
+          style={{ color: 'black' }}
+          href='/'
+        >
+          Inicio de sección
+        </a>
+        <a
+          className='nav-link btn btn-outline-info action-button'
+          style={{ color: 'black', marginLeft: '5px' }}
+          href='/'
+        >
+          Registrarse
+        </a>
+      </div>
+    </nav>
+  );
+};
